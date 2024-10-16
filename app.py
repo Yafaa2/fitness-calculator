370108d12bf929ef0cb363fffdb9492f36ea4c31
Khaled <khaledkhedr31@gmail.com>
2024-10-16 10:40:50 +0300

    feat(Calroiescalculator): Implemented Calories calculator endpoint

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
-"""app Backend"""
-
-
+"""Main-page endpoints and Backend"""
 from flask import Flask, request, jsonify, render_template
+from auth import auth
 
 app = Flask(__name__)
+
+app.register_blueprint(auth)
+
+class GenderSwitch:
+    """To calculate calories based on gender"""
+
+    def __init__(self):
+        self.activity_constants = {
+            "sedentary": 1.2,
+            "light": 1.375,
+            "moderate": 1.55,
+            "active": 1.725,
+            "super": 1.9
+        }
+
+    def calculate_bmr(self, age, height, weight, gender):
+        """Base BMR calculation for male and female"""
+        if gender.lower() == "male":
+            return (10 * weight) + (6.25 * height) - (5 * age) + 5
+
+        return (10 * weight) + (6.25 * height) - (5 * age) - 161
+
+    def calculate_tdee(self, bmr, activity_level):
+        """Calculate Total Daily Energy Expenditure based on activity level"""
+        activity_level_key = activity_level.lower().split(":")[0]
+        return bmr * self.activity_constants.get(activity_level_key, 1.2)
 
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
@@ -36,6 +61,41 @@ def calculate_bmi():
         "bmi": round(bmi, 2),
         "classification": classification
     })
+
+
+@app.route('/calculate-calories', methods=['GET', 'POST'])
+def calculate_calories():
+    """Calories Calculator endpoint"""
+    main_page()
+    user_data = request.json
+    age = user_data['age']
+    gender = user_data['gender']
+    height = user_data['height']
+    weight = user_data['weight']
+    activity_level = user_data['activityLevel']
+
+    if None in [age, gender, height, weight, activity_level]:
+        return jsonify({"error": "Missing age, gender, height, weight, or activity level"}), 400
+
+    gender_switch = GenderSwitch()
+
+    # Calculate BMR based on gender
+    bmr = gender_switch.calculate_bmr(age, height, weight, gender)
+
+    # Calculate TDEE based on activity level
+    tdee = gender_switch.calculate_tdee(bmr, activity_level)
+
+    to_lose_weight = tdee - 500
+    to_maintain_weight = tdee
+    to_gain_weight = tdee + 500
+
+    return jsonify({
+        "To Lose Weight": round(to_lose_weight, 2),
+        "To Maintain Weight": round(to_maintain_weight, 2),
+        "To Gain Weight": round(to_gain_weight, 2)
+    })
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
